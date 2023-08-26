1c1f8570db97a1a36c2a8e5db148a13deaddf11d
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2023-08-26 19:20:13 +0330

    third-version

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,8 +1,8 @@
-import React, { Component, createRef } from "react"
-import styled from "styled-components"
-import { Motion, spring } from "react-motion"
+import React, { Component, createRef } from 'react';
+import styled from 'styled-components';
+import { Motion, spring } from 'react-motion';
 
-import "./App.css"
+import './App.css';
 
 const Drawer = styled.div`
   position: absolute;
@@ -10,36 +10,42 @@ const Drawer = styled.div`
   background: #87ceeb;
   width: 30vh;
   left: ${(props) => props.left}vh;
-`
+`;
 
 const Link = styled.div`
   cursor: pointer;
   padding: 1em 1em 1em 3em;
   color: white;
   font-weight: bold;
-`
+`;
 
 class Hamburger extends Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.myRef = createRef();
-    
+
     this.state = {
       toggleState: 0,
-    }
-    this.handleClick = this.handleClick.bind(this)
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {    
-    this.setState(prevState => ({toggleState: prevState.toggleState ? 0 : 1}))
+  handleClick() {
+    this.setState((prevState) => ({
+      toggleState: prevState.toggleState ? 0 : 1,
+    }));
   }
 
   render() {
     return (
       <div id="parent">
         <div id="nav-bar">
-          <div className="container" ref={this.myRef} onClick={this.handleClick}>
+          <div
+            className="container"
+            ref={this.myRef}
+            onClick={this.handleClick}
+          >
             <div className="bar1" />
             <div className="bar2" />
             <div className="bar3" />
@@ -64,7 +70,7 @@ class Hamburger extends Component {
           )}
         </Motion>
       </div>
-    )
+    );
   }
 }
-export default Hamburger
+export default Hamburger;
